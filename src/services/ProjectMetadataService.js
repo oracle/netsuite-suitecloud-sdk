02786c05665e271615bd78c6d2b9a11b60603d35
Perez, Luis <luperez@netsuite.com>
2019-04-01 15:15:33 +0200

    Fix constant

--- src/services/ProjectMetadataService.js
+++ src/services/ProjectMetadataService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { ERRORS } = require('../services/TranslationKeys');
-const { MANIFEST_XML, XML } = require('../ApplicationConstants');
+const { MANIFEST_XML, MANIFEST_XML_DEFINITIONS } = require('../ApplicationConstants');
 const { PROJECT_SUITEAPP, PROJECT_ACP } = require('../ApplicationConstants');
 const CLIException = require('../CLIException');
 const FileUtils = require('../utils/FileUtils');
@@ -13,13 +13,13 @@ module.exports = class ProjectMetadataService {
 	_validate_xml(key, currentValue, newValue) {
 		//TODO Add more cases
 		if (key === '/manifest') {
-			if (!newValue['$'] || !newValue['$'][XML.ATTRIBUTES.PROJECT_TYPE]) {
+			if (!newValue['$'] || !newValue['$'][MANIFEST_XML_DEFINITIONS.ATTRIBUTES.PROJECT_TYPE]) {
 				throw new xml2js.ValidationError(
 					TranslationService.getMessage(ERRORS.XML_PROJECTTYPE_ATTRIBUTE_MISSING)
 				);
 			} else if (
-				newValue['$'][XML.ATTRIBUTES.PROJECT_TYPE] !== PROJECT_SUITEAPP &&
-				newValue['$'][XML.ATTRIBUTES.PROJECT_TYPE] !== PROJECT_ACP
+				newValue['$'][MANIFEST_XML_DEFINITIONS.ATTRIBUTES.PROJECT_TYPE] !== PROJECT_SUITEAPP &&
+				newValue['$'][MANIFEST_XML_DEFINITIONS.ATTRIBUTES.PROJECT_TYPE] !== PROJECT_ACP
 			) {
 				throw new xml2js.ValidationError(
 					TranslationService.getMessage(ERRORS.XML_PROJECTTYPE_INCORRECT)
@@ -39,7 +39,7 @@ module.exports = class ProjectMetadataService {
 			);
 
 		const manifestString = FileUtils.readAsString(manifestPath);
-		const beginTag = '<' + XML.TAGS.MANIFEST;
+		const beginTag = '<' + MANIFEST_XML_DEFINITIONS.TAGS.MANIFEST;
 		if (!manifestString.substr(0, beginTag.length) === beginTag) {
 			if (!result.manifest) {
 				throw new CLIException(
