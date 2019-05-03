929394c4bdd406f1ccb304df052d3a1c2ff3eedc
Gonzalez, Matias <matgonzalez@netsuite.com>
2019-05-03 11:46:20 -0300

    [SCASCS-5027] resources object

--- src/sdf-cli-local/src/services/Who.js
+++ src/sdf-cli-local/src/services/Who.js
@@ -17,53 +17,55 @@ class Who {
 		const host = this.req.get('host');
 		const app = this.req.params.app;
 
-		this.css = {
-			tag: 'link', 
-			resource: 'css', 
-			url: `${protocol}://${host}/css/${app}.css`
-		};
-		this.define_patch = {
-			tag: 'script', 
-			resource: 'define_patch', 
-			url: `${protocol}://${host}/define_patch.js`
-		};
-		this.requirejs = {
-			tag: 'script', 
-			resource: 'requirejs', 
-			url: `${protocol}://${host}/javascript/require.js`
-		};
-		this.javascript_libs = {
-			tag: 'script', 
-			resource: 'javascript_libs', 
-			url: 'javascript-libs.js'
-		};
-		this.templates = {
-			tag: 'script', 
-			resource: 'templates', 
-			url: `${protocol}://${host}/templates/${app}-templates.js`
-		};
-		this.js_core = {
-			tag: 'script', 
-			resource: 'js_core', 
-			url: null
-		};
-		this.js_extensions = {
-			tag: 'script', 
-			resource: 'js_extensions', 
-			url: `${protocol}://${host}/extensions/${app}_ext.js`
-		};
+		this.resources = {
+			css: {
+				tag: 'link',
+				resource: 'css',
+				url: `${protocol}://${host}/css/${app}.css`
+			},
+			define_patch: {
+				tag: 'script',
+				resource: 'define_patch',
+				url: `${protocol}://${host}/define_patch.js`
+			},
+			requirejs: {
+				tag: 'script',
+				resource: 'requirejs',
+				url: `${protocol}://${host}/javascript/require.js`
+			},
+			javascript_libs: {
+				tag: 'script',
+				resource: 'javascript_libs',
+				url: 'javascript-libs.js'
+			},
+			templates: {
+				tag: 'script',
+				resource: 'templates',
+				url: `${protocol}://${host}/templates/${app}-templates.js`
+			},
+			js_core: {
+				tag: 'script',
+				resource: 'js_core',
+				url: null
+			},
+			js_extensions: {
+				tag: 'script',
+				resource: 'js_extensions',
+				url: `${protocol}://${host}/extensions/${app}_ext.js`
+			}
+		}
 	}
 
 	setResponse() {
 
 		const response = [
-			this.css, 
-			this.requirejs, 
-			this.define_patch, 
-			this.javascript_libs, 
-			this.templates, 
-			this.js_core, 
-			this.js_extensions
+			this.resources.css, 
+			this.resources.requirejs, 
+			this.resources.define_patch, 
+			this.resources.javascript_libs, 
+			this.resources.templates, 
+			this.resources.js_core, 
+			this.resources.js_extensions
 		];
 
 		this.res.setHeader('Content-Type', 'application/json');
