789c23ad2d0dca8db9819abf7f83a49e4d4d2d11
Rebolleda, David <drebolleda@netsuite.com>
2019-05-08 19:12:47 +0200

    importfiles: formating and cleaning use ImportObjectsCommandGenerator for non interactive mode

--- src/commands/ImportFilesCommandGenerator.js
+++ src/commands/ImportFilesCommandGenerator.js
@@ -4,17 +4,13 @@ const BaseCommandGenerator = require('./BaseCommandGenerator');
 const CommandUtils = require('../utils/CommandUtils');
 const SDKExecutionContext = require('../SDKExecutionContext');
 const TranslationService = require('../services/TranslationService');
-const executeWithSpinner = require('../ui/CliSpinner').executeWithSpinner;
+const { executeWithSpinner } = require('../ui/CliSpinner');
 const NodeUtils = require('../utils/NodeUtils');
 const SDKOperationResultUtils = require('../utils/SDKOperationResultUtils');
 const ProjectMetadataService = require('../services/ProjectMetadataService');
 const { PROJECT_SUITEAPP } = require('../ApplicationConstants');
 const {
-	COMMAND_IMPORTFILES: {
-		ERRORS,
-		QUESTIONS,
-		MESSAGES,
-	},
+	COMMAND_IMPORTFILES: { ERRORS, QUESTIONS, MESSAGES },
 } = require('../services/TranslationKeys');
 
 const SUITE_SCRIPTS_FOLDER = '/SuiteScripts';
@@ -43,91 +39,84 @@ module.exports = class ImportFilesCommandGenerator extends BaseCommandGenerator 
 			return Promise.reject(TranslationService.getMessage(ERRORS.IS_SUITEAPP));
 		}
 
-		try {
-			const executionContextListFolders = new SDKExecutionContext({
-				command: COMMAND_NAMES.LISTFOLDERS,
-				showOutput: false,
-			});
-			this._applyDefaultContextParams(executionContextListFolders);
+		const executionContextListFolders = new SDKExecutionContext({
+			command: COMMAND_NAMES.LISTFOLDERS,
+			showOutput: false,
+		});
+		this._applyDefaultContextParams(executionContextListFolders);
 
-			const resultListFolders = await executeWithSpinner({
-				action: this._sdkExecutor.execute(executionContextListFolders),
-				message: TranslationService.getMessage(MESSAGES.LOADING_FOLDERS),
-			});
+		const listFoldersResult = await executeWithSpinner({
+			action: this._sdkExecutor.execute(executionContextListFolders),
+			message: TranslationService.getMessage(MESSAGES.LOADING_FOLDERS),
+		});
 
-			const firstAnswers = await prompt([
-				{
-					type: CommandUtils.INQUIRER_TYPES.LIST,
-					name: ANSWER_NAMES.FOLDER,
-					message: TranslationService.getMessage(QUESTIONS.SELECT_FOLDER),
-					default: SUITE_SCRIPTS_FOLDER,
-					choices: this._getFileCabinetFolders(resultListFolders),
-				},
-			]);
-
-			// quote folder path to preserve spaces
-			firstAnswers.folder = `\"${firstAnswers.folder}\"`;
-			const executionContextListFiles = new SDKExecutionContext({
-				command: COMMAND_NAMES.LISTFILES,
-				params: firstAnswers,
-			});
-			this._applyDefaultContextParams(executionContextListFiles);
-
-			const listFilesResult = await executeWithSpinner({
-				action: this._sdkExecutor.execute(executionContextListFiles),
-				message: TranslationService.getMessage(MESSAGES.LOADING_FILES),
-			});
-
-			if (SDKOperationResultUtils.hasErrors(listFilesResult)) {
-				return Promise.reject(SDKOperationResultUtils.getMessagesString(listFilesResult));
-			}
-			SDKOperationResultUtils.logMessages(listFilesResult);
-			if (Array.isArray(listFilesResult.data) && listFilesResult.data.length === 0) {
-				return {
-					abortExecution: true,
-					message: SDKOperationResultUtils.getMessagesString(listFilesResult),
-					skipCommandOptionsValidation: true,
-					paths: '',
-				};
-			}
-
-			const questions = this._generateImportFilesQuestions(listFilesResult);
-
-			const secondAnswers = await prompt(questions);
-			return secondAnswers;
-		} catch (error) {
-			Promise.reject(
-				TranslationService.getMessage(ERRORS.INTERNAL, this._commandMetadata.name, error)
-			);
+		if (SDKOperationResultUtils.hasErrors(listFoldersResult)) {
+			return Promise.reject(SDKOperationResultUtils.getMessagesString(listFoldersResult));
 		}
+
+		const firstQuestion = this._generateSelectFolderQuestion(listFoldersResult);
+		const firstAnswer = await prompt([firstQuestion]);
+
+		// quote folder path to preserve spaces
+		firstAnswer.folder = `\"${firstAnswer.folder}\"`;
+		const executionContextListFiles = new SDKExecutionContext({
+			command: COMMAND_NAMES.LISTFILES,
+			params: firstAnswer,
+		});
+		this._applyDefaultContextParams(executionContextListFiles);
+
+		const listFilesResult = await executeWithSpinner({
+			action: this._sdkExecutor.execute(executionContextListFiles),
+			message: TranslationService.getMessage(MESSAGES.LOADING_FILES),
+		});
+
+		if (SDKOperationResultUtils.hasErrors(listFilesResult)) {
+			return Promise.reject(SDKOperationResultUtils.getMessagesString(listFilesResult));
+		}
+		if (Array.isArray(listFilesResult.data) && listFilesResult.data.length === 0) {
+			return Promise.reject(SDKOperationResultUtils.getMessagesString(listFilesResult));
+		}
+
+		const secondQuestion = this._generateSelectFilesQuestion(listFilesResult);
+		return await prompt([secondQuestion]);
 	}
 
 	_getFileCabinetFolders(listFoldersResponse) {
 		return listFoldersResponse.data.map(folder => ({
 			name: folder.path,
 			value: folder.path,
-			disabled: folder.isRestricted ? TranslationService.getMessage(MESSAGES.RESTRICTED_FOLDER) : '',
+			disabled: folder.isRestricted
+				? TranslationService.getMessage(MESSAGES.RESTRICTED_FOLDER)
+				: '',
 		}));
 	}
 
-	_generateImportFilesQuestions(listFilesResult) {
-		return [
-			{
-				type: CommandUtils.INQUIRER_TYPES.CHECKBOX,
-				name: ANSWER_NAMES.PATHS,
-				message: TranslationService.getMessage(QUESTIONS.SELECT_FILES),
-				choices: listFilesResult.data.map(path => ({ name: path, value: path })),
-				validate: fieldValue => showValidationResults(fieldValue, validateArrayIsNotEmpty),
-			},
-		];
+	_generateSelectFolderQuestion(listFoldersResult) {
+		return {
+			type: CommandUtils.INQUIRER_TYPES.LIST,
+			name: ANSWER_NAMES.FOLDER,
+			message: TranslationService.getMessage(QUESTIONS.SELECT_FOLDER),
+			default: SUITE_SCRIPTS_FOLDER,
+			choices: this._getFileCabinetFolders(listFoldersResult),
+		};
 	}
 
-	_preExecuteAction(args) {
-		args.project = this._projectFolder;
-		if (Array.isArray(args.paths)) {
-			args.paths = args.paths.join(' ');
+	_generateSelectFilesQuestion(listFilesResult) {
+		return {
+			type: CommandUtils.INQUIRER_TYPES.CHECKBOX,
+			name: ANSWER_NAMES.PATHS,
+			message: TranslationService.getMessage(QUESTIONS.SELECT_FILES),
+			choices: listFilesResult.data.map(path => ({ name: path, value: path })),
+			validate: fieldValue => showValidationResults(fieldValue, validateArrayIsNotEmpty),
+		};
+	}
+
+	_preExecuteAction(answers) {
+		answers.project = this._projectFolder;
+		if (Array.isArray(answers.paths)) {
+			answers.paths = answers.paths.join(' ');
 		}
-		return args;
+		return answers;
 	}
 
 	_executeAction(answers) {
@@ -159,12 +148,21 @@ module.exports = class ImportFilesCommandGenerator extends BaseCommandGenerator 
 		if (Array.isArray(data.results)) {
 			const successful = data.results.filter(result => result.loaded === true);
 			const unsuccessful = data.results.filter(result => result.loaded !== true);
-			successful.forEach(result => {
-				NodeUtils.println(result.path, NodeUtils.COLORS.RESULT);
-			});
-			unsuccessful.forEach(result => {
-				NodeUtils.println(result.path, NodeUtils.COLORS.WARNING);
-			});
+			if (successful.length) {
+				NodeUtils.println('The following objects were imported:', NodeUtils.COLORS.RESULT);
+				successful.forEach(result => {
+					NodeUtils.println(result.path, NodeUtils.COLORS.RESULT);
+				});
+			}
+			if (unsuccessful.length) {
+				NodeUtils.println(
+					'The following objects were NOT imported:',
+					NodeUtils.COLORS.WARNING
+				);
+				unsuccessful.forEach(result => {
+					NodeUtils.println(`${result.path} ${result.message}`, NodeUtils.COLORS.WARNING);
+				});
+			}
 		}
 	}
 };
