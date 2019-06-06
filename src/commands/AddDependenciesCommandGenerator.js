c3f37a0f743b5619f827bc4d7d2297a6d859ffd8
Octaviano III, Rodolfo R <roctaviano@netsuite.com>
2019-06-06 13:27:16 +0800

    PDPDEVTOOL-785: AddDependencies support - address code review comments

--- src/commands/AddDependenciesCommandGenerator.js
+++ src/commands/AddDependenciesCommandGenerator.js
@@ -37,10 +37,17 @@ const DEPENDENCY_TYPES = {
 const FEATURE_REQUIRED = 'required';
 const FEATURE_OPTIONAL = 'optional';
 
-const APP_ID_PREFIX = 'appId=';
-const BUNDLE_ID_PREFIX = 'bundleId=';
-const OBJECT_TYPE_PREFIX = 'objectType=';
-const SCRIPT_ID_PREFIX = 'scriptId=';
+const OBJECT_REFERENCE_ATTRIBUTES = {
+	APP_ID: 'appId=',
+	BUNDLE_ID: 'bundleId=',
+	OBJECT_TYPE: 'objectType=',
+	SCRIPT_ID: 'scriptId=',
+};
+
+const OBJECT_CONTAINER_PREFIX = {
+	BUNDLE: 'Bundle',
+	SUITEAPP: 'Application',
+};
 
 const COMMAND_OPTIONS = {
 	ALL: 'all',
@@ -51,16 +58,14 @@ module.exports = class AddDependenciesCommandGenerator extends BaseCommandGenera
 	constructor(options) {
 		super(options);
 	}
+
 	_executeAction(answers) {
 		answers[COMMAND_OPTIONS.PROJECT] = this._projectFolder;
-
-		let flags = [];
-		flags.push(COMMAND_OPTIONS.ALL);
 
 		const executionContext = new SDKExecutionContext({
 			command: this._commandMetadata.name,
 			params: answers,
-			flags: flags,
+			flags: [COMMAND_OPTIONS.ALL],
 		});
 
 		return executeWithSpinner({
@@ -69,8 +74,12 @@ module.exports = class AddDependenciesCommandGenerator extends BaseCommandGenera
 		});
 	}
 
-	_getDependenciesString(data) {
-		let dependenciesString = [];
+	_supportsInteractiveMode() {
+		return false;
+	}
+
+	_getDependenciesStringsArray(data) {
+		const dependenciesString = [];
 		//Features
 		const features = data.filter(
 			dependency => dependency.type === DEPENDENCY_TYPES.FEATURE.name
@@ -97,29 +106,37 @@ module.exports = class AddDependenciesCommandGenerator extends BaseCommandGenera
 		//Objects - Regular, SuiteApp,  Bundle dependencies
 		const objects = data.filter(dependency => dependency.type === DEPENDENCY_TYPES.OBJECT.name);
 		objects.forEach(object => {
-			const appIdDisplay = object.appId ? `${APP_ID_PREFIX}${object.appId}, ` : '';
-			const bundleIdDisplay = object.bundleIds
-				? `${BUNDLE_ID_PREFIX}${object.bundleIds}, `
+			const appIdDisplay = object.appId
+				? `in [${OBJECT_CONTAINER_PREFIX.SUITEAPP} - ${OBJECT_REFERENCE_ATTRIBUTES.APP_ID}${
+						object.appId
+				  }]`
 				: '';
-			const scriptIdDisplay = `${SCRIPT_ID_PREFIX}${object.scriptId}`;
+			const bundleIdDisplay = object.bundleIds
+				? `in [${OBJECT_CONTAINER_PREFIX.BUNDLE} - ${
+						OBJECT_REFERENCE_ATTRIBUTES.BUNDLE_ID
+				  }${object.bundleIds}]`
+				: '';
+			const scriptIdDisplay = `${OBJECT_REFERENCE_ATTRIBUTES.SCRIPT_ID}${object.scriptId}`;
 			dependenciesString.push(
-				`${
+				`[${
 					DEPENDENCY_TYPES.OBJECT.prefix
-				} ${appIdDisplay}${bundleIdDisplay}${scriptIdDisplay}`
+				} ${scriptIdDisplay}] ${appIdDisplay}${bundleIdDisplay}`
 			);
 		});
 
-		//Platform Extension
+		//Platform Extensions
 		const platforExtensions = data.filter(
 			dependency => dependency.type === DEPENDENCY_TYPES.PLATFORMEXTENSION.name
 		);
 		platforExtensions.forEach(platforExtension => {
 			const appIdDisplay = platforExtension.appId
-				? `${APP_ID_PREFIX}${platforExtension.appId}, `
+				? `${OBJECT_REFERENCE_ATTRIBUTES.APP_ID}${platforExtension.appId}, `
 				: '';
-			const objectTypeDisplay = `${OBJECT_TYPE_PREFIX}${platforExtension.objectType}`;
+			const objectTypeDisplay = `${OBJECT_REFERENCE_ATTRIBUTES.OBJECT_TYPE}${
+				platforExtension.objectType
+			}`;
 			dependenciesString.push(
-				`${DEPENDENCY_TYPES.OBJECT.prefix} ${appIdDisplay}${objectTypeDisplay}`
+				`${DEPENDENCY_TYPES.PLATFORMEXTENSION.prefix} ${appIdDisplay}${objectTypeDisplay}`
 			);
 		});
 
@@ -127,11 +144,8 @@ module.exports = class AddDependenciesCommandGenerator extends BaseCommandGenera
 	}
 
 	_formatOutput(operationResult) {
-		if (!operationResult) {
-			return;
-		}
 		if (SDKOperationResultUtils.hasErrors(operationResult)) {
-			SDKOperationResultUtils.logErrors(operationResultt);
+			SDKOperationResultUtils.logErrors(operationResult);
 			return;
 		}
 
@@ -151,9 +165,8 @@ module.exports = class AddDependenciesCommandGenerator extends BaseCommandGenera
 			NodeUtils.COLORS.RESULT
 		);
 
-		let dependenciesString = this._getDependenciesString(data);
-		dependenciesString.sort().forEach(output => {
-			NodeUtils.println(output, NodeUtils.COLORS.RESULT);
-		});
+		this._getDependenciesStringsArray(data)
+			.sort()
+			.forEach(output => NodeUtils.println(output, NodeUtils.COLORS.RESULT));
 	}
 };
