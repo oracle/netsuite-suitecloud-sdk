013762408db69f9a7b3fefdefa8692a7a5497497
Balbas, Javier <jbalbas@netsuite.com>
2020-03-10 16:17:58 +0100

    PDPDEVTOOL-2835 - Adding missing TestFrameworkException file

---
+++ packages/unit-testing/TestFrameworkException.js
@@ -0,0 +1,22 @@
+/*
+** Copyright (c) 2020 Oracle and/or its affiliates.  All rights reserved.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+'use strict';
+
+module.exports = class TestFrameworkException {
+	constructor(code, defaultMessage, infoMessage, translationKey) {
+		this._code = code;
+		this._defaultMessage = defaultMessage;
+		this._infoMessage = infoMessage;
+		this._translationKey = translationKey;
+	}
+
+	getInfoMessage() {
+		return this._infoMessage;
+	}
+
+	getErrorMessage() {
+		return this._defaultMessage;
+	}
+};
