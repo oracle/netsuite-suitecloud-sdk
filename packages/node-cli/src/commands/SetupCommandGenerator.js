68d8c0b6c7bb25e480be65e2d5fd617ef1c88ac3
Perez, Luis <lfpvillegas@gmail.com>
2020-06-08 15:59:10 +0200

    Fix

--- packages/node-cli/src/commands/SetupCommandGenerator.js
+++ packages/node-cli/src/commands/SetupCommandGenerator.js
@@ -166,7 +166,7 @@ module.exports = class SetupCommandGenerator extends BaseCommandGenerator {
 							fieldValue,
 							validateFieldIsNotEmpty,
 							validateFieldHasNoSpaces,
-							(fieldValue) => validateAuthIDNotInList(fieldValue, authIDList.data),
+							(fieldValue) => validateAuthIDNotInList(fieldValue, authIDList),
 							validateAlphanumericHyphenUnderscore,
 							validateMaximumLength
 						),
