e26c4c16b5e5195a1fdee8df64be1c5b4daa8cec
Perez, Luis <lfpvillegas@gmail.com>
2020-08-28 14:39:32 +0200

    Fix error and recover property's use isJavaVersionValid

--- packages/node-cli/src/SdkExecutor.js
+++ packages/node-cli/src/SdkExecutor.js
@@ -57,7 +57,6 @@ module.exports = class SdkExecutor {
 		this.childProcess.stderr.on(DATA_EVENT, (data) => {
 			lastSdkError += data.toString(UTF8);
 		});
-
 		this.childProcess.stdout.on(DATA_EVENT, (data) => {
 			lastSdkOutput += data.toString(UTF8);
 		});
@@ -74,15 +73,21 @@ module.exports = class SdkExecutor {
 					reject(NodeTranslationService.getMessage(ERRORS.SDKEXECUTOR.RUNNING_COMMAND, error));
 				}
 			} else {
+				const javaVersionError = this._checkIfJavaVersionIssue();
+				if (javaVersionError) {
+					reject(javaVersionError);
+				}
 				reject(NodeTranslationService.getMessage(ERRORS.SDKEXECUTOR.SDK_ERROR, code, lastSdkError));
 			}
 		});
 	}
 
 	_launchJvmCommand(executionContext) {
-		const javaVersionError = this._checkIfJavaVersionIssue();
-		if (javaVersionError) {
-			throw javaVersionError;
+		if (!this._CLISettingsService.isJavaVersionValid()) {
+			const javaVersionError = this._checkIfJavaVersionIssue();
+			if (javaVersionError) {
+				throw javaVersionError;
+			}
 		}
 
 		const proxyOptions = this._getProxyOptions();
