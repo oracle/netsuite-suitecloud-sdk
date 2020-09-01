22166608428902cdf8d811ab3c59b0c64ccbf1be
Perez, Luis <lfpvillegas@gmail.com>
2020-09-01 18:12:41 +0200

    Added ability to modify sdkcommandmetadata.

--- packages/node-cli/src/core/CommandsMetadataService.js
+++ packages/node-cli/src/core/CommandsMetadataService.js
@@ -6,7 +6,12 @@
 
 const path = require('path');
 const FileUtils = require('../utils/FileUtils');
-const { SDK_COMMANDS_METADATA_FILE, NODE_COMMANDS_METADATA_FILE, COMMAND_GENERATORS_METADATA_FILE } = require('../ApplicationConstants');
+const {
+	SDK_COMMANDS_METADATA_FILE,
+	MODIFIED_SDK_DESCRIPTIONS_FOR_NODECLI_METADATA_FILE: SDK_COMMANDS_MODIFIED_METADATA_FILE,
+	NODE_COMMANDS_METADATA_FILE,
+	COMMAND_GENERATORS_METADATA_FILE,
+} = require('../ApplicationConstants');
 const SDK_WRAPPER_GENERATOR = 'commands/SdkWrapperCommandGenerator';
 
 let commandsMetadataCache;
@@ -29,15 +34,50 @@ module.exports = class CommandsMetadataService {
 	_initializeCommandsMetadata() {
 		if (!commandsMetadataCache) {
 			const sdkCommandsMetadata = this._getMetadataFromFile(path.join(this._rootCLIPath, SDK_COMMANDS_METADATA_FILE));
+			const modifiedSdkCommandsMetadata = this._getMetadataFromFile(path.join(this._rootCLIPath, SDK_COMMANDS_MODIFIED_METADATA_FILE));
 			const nodeCommandsMetadata = this._getMetadataFromFile(path.join(this._rootCLIPath, NODE_COMMANDS_METADATA_FILE));
 			const commandGeneratorsMetadata = this._getMetadataFromFile(path.join(this._rootCLIPath, COMMAND_GENERATORS_METADATA_FILE));
+
+			let combinedSdkCommandMetadata = this._combineMetadata(sdkCommandsMetadata, modifiedSdkCommandsMetadata);
 			let combinedMetadata = {
-				...sdkCommandsMetadata,
+				...combinedSdkCommandMetadata,
 				...nodeCommandsMetadata,
 			};
-			combinedMetadata = this._transformCommandsOptionsToObject(combinedMetadata);
 			combinedMetadata = this._addCommandGeneratorMetadata(commandGeneratorsMetadata, combinedMetadata);
 			commandsMetadataCache = combinedMetadata;
+		}
+	}
+
+	_combineMetadata(sdkCommandsMetadata, modifiedSdkCommandsMetadata) {
+		let combinedMetadata = sdkCommandsMetadata;
+		Object.entries(modifiedSdkCommandsMetadata).forEach((entry) => {
+			const [command, properties] = entry;
+			let originalCommand = sdkCommandsMetadata[command];
+			let newCommand;
+			if (originalCommand) {
+				newCommand = this._replaceCommandProperties(originalCommand, properties);
+			} else {
+				newCommand = properties;
+			}
+			combinedMetadata[command] = newCommand;
+		});
+		return combinedMetadata;
+	}
+
+	_replaceCommandProperties(originalCommand, properties) {
+		let newCommand = originalCommand;
+		Object.entries(properties).forEach((entry) => {
+			const [property, newPropertyValue] = entry;
+			newCommand[property] = this._replaceProperty(originalCommand[property], newPropertyValue);
+		});
+		return newCommand;
+	}
+
+	_replaceProperty(originalPropertyValue, newPropertyValue) {
+		if (typeof newPropertyValue === 'object') {
+			return this._replaceCommandProperties(originalPropertyValue, newPropertyValue);
+		} else {
+			return newPropertyValue;
 		}
 	}
 
@@ -64,23 +104,9 @@ module.exports = class CommandsMetadataService {
 		}
 	}
 
-	_transformCommandsOptionsToObject(commandsMetadata) {
-		executeForEachCommandMetadata(commandsMetadata, commandMetadata => {
-			const optionsTransformedIntoObject = commandMetadata.options.reduce((result, item) => {
-				if (item.name == null) {
-					throw 'Invalid Metadata, missing "name" property in command options';
-				}
-				result[item.name] = item;
-				return result;
-			}, {});
-			commandMetadata.options = optionsTransformedIntoObject;
-		});
-		return commandsMetadata;
-	}
-
 	_addCommandGeneratorMetadata(commandGeneratorsMetadata, commandsMetadata) {
-		executeForEachCommandMetadata(commandsMetadata, commandMetadata => {
-			const generatorMetadata = commandGeneratorsMetadata.find(generatorMetadata => {
+		executeForEachCommandMetadata(commandsMetadata, (commandMetadata) => {
+			const generatorMetadata = commandGeneratorsMetadata.find((generatorMetadata) => {
 				return generatorMetadata.commandName === commandMetadata.name;
 			});
 			commandMetadata.generator = path.join(this._rootCLIPath, generatorMetadata.generator);
