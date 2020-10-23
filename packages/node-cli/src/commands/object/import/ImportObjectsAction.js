a0301ed78889c7e62e5d52447139bb652b2f35ca
Karl Anthony Ng <karl.anthony.ng@oracle.com>
2020-10-23 18:09:22 +0800

    localserver-cli-command-update-package-lock-json

--- packages/node-cli/src/commands/object/import/ImportObjectsAction.js
+++ packages/node-cli/src/commands/object/import/ImportObjectsAction.js
@@ -75,21 +75,26 @@ module.exports = class ImportObjectsAction extends BaseAction {
 
 				scriptIdArray = params[ANSWERS_NAMES.SCRIPT_ID].split(' ');
 			} else {
-				this._log.info(NodeTranslationService.getMessage(WARNINGS.OVERRIDE));
 				if (params[COMMAND_FLAGS.EXCLUDE_FILES]) {
 					flags.push(COMMAND_FLAGS.EXCLUDE_FILES);
 					delete params[COMMAND_FLAGS.EXCLUDE_FILES];
 				}
 
 				if (params[ANSWERS_NAMES.OBJECT_TYPE] === IMPORT_OBJECTS_COMMAND_TYPE_PARAM_ALL) {
-					scriptIdArray = (await this._getAllScriptIds(params)).map((el) => el.scriptId);
-				} else if (params[ANSWERS_NAMES.OBJECT_TYPE] !== IMPORT_OBJECTS_COMMAND_TYPE_PARAM_ALL) {
+					if (params[ANSWERS_NAMES.SCRIPT_ID] === IMPORT_OBJECTS_COMMAND_SCRIPT_ID_PARAM_ALL) {
+						scriptIdArray = (await this._getAllScriptIds(params)).map((el) => el.scriptId);
+					} else {
+						scriptIdArray = (await this._getScriptIdsForAllObjectTypesFilteredByScriptId(params)).map((el) => el.scriptId);
+					}
+				} else {
 					if (params[ANSWERS_NAMES.SCRIPT_ID] === IMPORT_OBJECTS_COMMAND_SCRIPT_ID_PARAM_ALL) {
 						scriptIdArray = (await this._getAllScriptIdsForObjectType(params)).map((el) => el.scriptId);
 					} else {
 						scriptIdArray = (await this._getScriptIdsForObjectTypeFilteredByScriptId(params)).map((el) => el.scriptId);
 					}
 				}
+
+				this._log.info(NodeTranslationService.getMessage(WARNINGS.OVERRIDE));
 			}
 
 			delete params[ANSWERS_NAMES.SCRIPT_ID];
@@ -169,6 +174,13 @@ module.exports = class ImportObjectsAction extends BaseAction {
 	async _getAllScriptIdsForObjectType(params) {
 		const sdkParams = {};
 		sdkParams.type = params[ANSWERS_NAMES.OBJECT_TYPE];
+
+		return this._callListObjects(params, sdkParams);
+	}
+
+	async _getScriptIdsForAllObjectTypesFilteredByScriptId(params) {
+		const sdkParams = {};
+		sdkParams.scriptid = params[ANSWERS_NAMES.SCRIPT_ID];
 
 		return this._callListObjects(params, sdkParams);
 	}
