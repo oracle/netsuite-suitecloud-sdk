1e79041c8798f12ce8d05834f84af9f73c73fdd3
Perez, Luis <lfpvillegaso@gmail.com>
2021-02-02 18:04:05 +0100

    Make eslint work for the specific project

--- packages/vscode-extension/.eslintrc.js
+++ packages/vscode-extension/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "project": "tsconfig.json",
+        "tsconfigRootDir": __dirname,
         "sourceType": "module"
     },
     "plugins": [
