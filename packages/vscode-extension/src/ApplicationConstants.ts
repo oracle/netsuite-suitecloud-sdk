627dac29800a0c663ba1b6b9c1ff61327668538b
Rebolleda, David <drebolleda@netsuite.com>
2021-02-22 18:11:38 +0100

    modify .jar reference to 21.1 in constants modifify version and sdkDownloadUrl in package.json modify netsuite_version reference in CLI.js delete files not needed in release/21.1

--- packages/vscode-extension/src/ApplicationConstants.ts
+++
@@ -1,8 +0,0 @@
-/*
- ** Copyright (c) 2021 Oracle and/or its affiliates.  All rights reserved.
- ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
- */
-
-export const OBJECTS_FOLDER = "Objects";
-export const PRODUCTION_DOMAIN_REGEX = '^system\\.(\\w+\\.)*netsuite\\.com$';
-export const PRODUCTION_ACCOUNT_SPECIFIC_DOMAIN_REGEX = '^(\\w+)\\.app\\.netsuite\\.com$';
