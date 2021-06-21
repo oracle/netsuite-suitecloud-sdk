fef3aa9973e0b474f0f67e99ce4181d0d6abf2a1
Perez, Luis <luperez@netsuite.com>
2021-06-21 13:38:09 +0200

    Delete comment

--- packages/vscode-extension/src/service/ImportFileService.ts
+++ packages/vscode-extension/src/service/ImportFileService.ts
@@ -6,7 +6,6 @@ import VSConsoleLogger from '../loggers/VSConsoleLogger';
 const COMMAND_NAME = 'file:import';
 
 export default class ImportFileService {
-	
 	protected readonly translationService: VSTranslationService;
 	protected executionPath?: string;
 	protected readonly messageService: MessageService;
@@ -16,13 +15,18 @@ export default class ImportFileService {
 	constructor(messageService: MessageService, translationService: VSTranslationService, fsPath: string | undefined) {
 		this.messageService = messageService;
 		this.translationService = translationService;
-		// this.executionPath = getRootProjectFolder(fsPath)
 	}
 
-	async importFiles(selectedFilesPaths: string[], directory: string, statusBarMessage: string, executionPath: string | undefined, excludeProperties: boolean) {
+	async importFiles(
+		selectedFilesPaths: string[],
+		directory: string,
+		statusBarMessage: string,
+		executionPath: string | undefined,
+		excludeProperties: boolean
+	) {
 		this.executionPath = executionPath;
 		let commandArgs: any = { project: directory, paths: selectedFilesPaths };
-		
+
 		if (excludeProperties) {
 			commandArgs.excludeproperties = true;
 		}
@@ -33,8 +37,8 @@ export default class ImportFileService {
 	}
 
 	protected async runSuiteCloudCommand(args: { [key: string]: string } = {}) {
-		if(!this.vsConsoleLogger) {
-			throw Error("Logger not initialized");
+		if (!this.vsConsoleLogger) {
+			throw Error('Logger not initialized');
 		}
 		return new SuiteCloudRunner(this.vsConsoleLogger, this.executionPath).run({
 			commandName: COMMAND_NAME,
