3da07c55d519c14e098026d0232338ce1a96da77
Perez, Luis <luperez@netsuite.com>
2021-07-21 18:57:31 +0200

    Not show result of list objects in the console

--- packages/vscode-extension/src/service/ImportObjectService.ts
+++ packages/vscode-extension/src/service/ImportObjectService.ts
@@ -7,6 +7,8 @@ import VSConsoleLogger from '../loggers/VSConsoleLogger';
 import MessageService from './MessageService';
 import { IMPORT_OBJECTS, LIST_OBJECTS } from './TranslationKeys';
 import { VSTranslationService } from './VSTranslationService';
+import DummyConsoleLogger from '../loggers/DummyConsoleLogger';
+import { ConsoleLogger } from '../util/ExtensionUtil';
 
 const IMPORT_OBJECT_COMMAND_NAME = 'object:import';
 const LIST_OBJECT_COMMAND_NAME = 'object:list';
@@ -31,6 +33,7 @@ export default class CustomObjectService {
 		executionPath: string | undefined
 	) {
 		this.executionPath = executionPath;
+		//We choose 'ALL' types because it is chosen before which scriptIds should be imported and not by type
 		let commandArgs: any = { type: 'ALL', destinationfolder: destinationFolder };
 
 		commandArgs.scriptid = scriptIds && scriptIds.length > 0 ? scriptIds : 'ALL';
@@ -49,12 +52,7 @@ export default class CustomObjectService {
 		return await commandActionPromise;
 	}
 
-	async listObjects(
-		appId: string | undefined,
-		types: string[],
-		scriptId: string | undefined,
-		executionPath: string | undefined
-	) {
+	async listObjects(appId: string | undefined, types: string[], scriptId: string | undefined, executionPath: string | undefined) {
 		this.executionPath = executionPath;
 		let commandArgs: any = { type: types.join(' ') };
 
@@ -65,23 +63,24 @@ export default class CustomObjectService {
 			commandArgs.scriptid = scriptId;
 		}
 
-		const commandActionPromise = this.runSuiteCloudCommand(commandArgs, LIST_OBJECT_COMMAND_NAME);
+		const commandActionPromise = this.runSuiteCloudCommand(commandArgs, LIST_OBJECT_COMMAND_NAME, new DummyConsoleLogger());
 		const statusBarMessage = this.translationService.getMessage(LIST_OBJECTS.LISTING);
 		this.messageService.showStatusBarMessage(statusBarMessage, true, commandActionPromise);
 		return await commandActionPromise;
 	}
 
-	protected async runSuiteCloudCommand(args: { [key: string]: string } = {}, command: string) {
-		if (!this.vsConsoleLogger) {
+	protected async runSuiteCloudCommand(args: { [key: string]: string } = {}, command: string, consoleLogger?: typeof ConsoleLogger) {
+		const logger = consoleLogger ? consoleLogger : this.vsConsoleLogger;
+		if (!logger) {
 			throw Error(CONSOLE_LOGGER_ERROR);
 		}
-		return new SuiteCloudRunner(this.vsConsoleLogger, this.executionPath).run({
+		return new SuiteCloudRunner(logger, this.executionPath).run({
 			commandName: command,
 			arguments: args,
 		});
 	}
 
-	setVsConsoleLogger(vsConsoleLogger: VSConsoleLogger) {
+	setVsConsoleLogger(vsConsoleLogger: typeof ConsoleLogger) {
 		this.vsConsoleLogger = vsConsoleLogger;
 	}
 }
