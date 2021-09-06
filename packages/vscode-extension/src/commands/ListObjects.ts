6a3af961369e584ba8147470af5500528a219727
Karl Anthony Ng <karl.anthony.ng@oracle.com>
2021-09-06 13:27:34 +0800

    PDPDEVTOOL-4448: vscode - suiteapp - object list needs to ask if from specific suiteapp

--- packages/vscode-extension/src/commands/ListObjects.ts
+++ packages/vscode-extension/src/commands/ListObjects.ts
@@ -3,8 +3,8 @@
  ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
 import { window } from 'vscode';
-import { COMMAND, LIST_OBJECTS } from '../service/TranslationKeys';
-import { actionResultStatus, objectTypes } from '../util/ExtensionUtil';
+import { ANSWERS, COMMAND, LIST_OBJECTS } from '../service/TranslationKeys';
+import { InteractiveAnswersValidator, ProjectInfoService, actionResultStatus, objectTypes } from '../util/ExtensionUtil';
 import BaseAction from './BaseAction';
 
 const COMMAND_NAME = 'listobjects';
@@ -15,24 +15,39 @@ export default class ListObjects extends BaseAction {
 	}
 
 	protected async execute() {
-		const selectedObjectTypes = await window.showQuickPick(
-			objectTypes.map((objectType) => objectType.value.type),
-			{
-				placeHolder: 'select your object type or nothing to list them all',
-				canPickMany: true,
-			}
-		);
+		const commandArgs: { [key: string]: string | string[] } = {}
 
+		const projectInfoService = new ProjectInfoService(this.getProjectFolderPath());
+		if (projectInfoService.isSuiteAppProject()) {
+			const filterAppId = await this.promptFilterAppIdQuestion();
+			if (!filterAppId) {
+				return;
+			}
+
+			if (filterAppId === this.translationService.getMessage(ANSWERS.YES)) {
+				let appId = await this.promptAppIdQuestion(projectInfoService);
+				if (appId === undefined) {
+					return;
+				}
+				commandArgs.appid = appId;
+			}
+		}
+
+		const selectedObjectTypes = await this.promptSelectObjectTypesQuestion();
 		if (selectedObjectTypes === undefined) {
 			return;
 		}
-
-		let typeParam = {};
 		if (selectedObjectTypes.length !== 0) {
-			typeParam = { type: selectedObjectTypes };
+			commandArgs.types = selectedObjectTypes
 		}
 
-		const commandActionPromise = this.runSuiteCloudCommand(typeParam);
+		const scriptIdFilter = await this.promptFilterScriptIdQuestion();
+		if (scriptIdFilter === undefined) {
+			return;
+		}
+		commandArgs.scriptid = scriptIdFilter;
+
+		const commandActionPromise = this.runSuiteCloudCommand(commandArgs);
 		const commandMessage = this.translationService.getMessage(COMMAND.TRIGGERED, this.vscodeCommandName);
 		const statusBarMessage = this.translationService.getMessage(LIST_OBJECTS.LISTING);
 		this.messageService.showInformationMessage(commandMessage, statusBarMessage, commandActionPromise);
@@ -44,4 +59,59 @@ export default class ListObjects extends BaseAction {
 			this.messageService.showCommandError();
 		}
 	}
+
+	private async promptFilterScriptIdQuestion(): Promise<string | undefined> {
+		return window.showInputBox({
+			ignoreFocusOut: true,
+			placeHolder: this.translationService.getMessage(LIST_OBJECTS.QUESTIONS.FILTER_SCRIPT_ID),
+			validateInput: (fieldValue) => {
+				if (fieldValue === '') {
+					return null;
+				}
+				let validationResult = InteractiveAnswersValidator.showValidationResults(fieldValue, InteractiveAnswersValidator.validateScriptId);
+				return typeof validationResult === 'string' ? validationResult : null;
+			},
+		});
+	}
+
+	private async promptSelectObjectTypesQuestion(): Promise<string[] | undefined> {
+		return window.showQuickPick(
+			objectTypes.map((objectType) => objectType.value.type),
+			{
+				placeHolder: this.translationService.getMessage(LIST_OBJECTS.QUESTIONS.SELECT_OBJECTS_TYPES),
+				canPickMany: true,
+			}
+		);
+	}
+
+	private async promptFilterAppIdQuestion(): Promise<string | undefined> {
+		return window.showQuickPick([this.translationService.getMessage(ANSWERS.YES), this.translationService.getMessage(ANSWERS.NO)], {
+			placeHolder: this.translationService.getMessage(LIST_OBJECTS.QUESTIONS.FILTER_APP_ID),
+			canPickMany: false,
+		});
+	}
+
+	private async promptAppIdQuestion(projectInfoService: typeof ProjectInfoService): Promise<string | undefined> {
+		const defaultAppId = projectInfoService.getApplicationId();
+		let appId = await window.showInputBox({
+			ignoreFocusOut: true,
+			placeHolder: this.translationService.getMessage(LIST_OBJECTS.QUESTIONS.APP_ID, defaultAppId),
+			validateInput: (fieldValue) => {
+				if (fieldValue === '') {
+					return null;
+				}
+				let validationResult = InteractiveAnswersValidator.showValidationResults(fieldValue, InteractiveAnswersValidator.validateSuiteApp);
+				return typeof validationResult === 'string' ? validationResult : null;
+			},
+		});
+		if (appId === undefined) {
+			return;
+		}
+
+		if (appId === '') {
+			appId = defaultAppId;
+		}
+		return appId;
+	}
+
 }
