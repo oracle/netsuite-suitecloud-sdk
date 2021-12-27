0076d55603d0b82b6c7b230e3b457063c2150da3
Rebolleda, David <david.rebolleda@oracle.com>
2021-12-27 12:45:23 +0100

    Revert "updatate refrence to future Netsuite version"
    This reverts commit 99c77848d474ea36be30a65be1e4fc681853f4c9.

--- packages/node-cli/src/ApplicationConstants.js
+++ packages/node-cli/src/ApplicationConstants.js
@@ -9,8 +9,8 @@ module.exports = {
 	SDK_COMMANDS_METADATA_PATCH_FILE: 'metadata/SdkCommandsMetadataPatch.json', //Overwrites selected properties in SdkCommandsMetadata.json file that are specific for SuiteCloud CLI for Node.js
 	NODE_COMMANDS_METADATA_FILE: 'metadata/NodeCommandsMetadata.json',
 	COMMAND_GENERATORS_METADATA_FILE: 'metadata/CommandGenerators.json',
-	COMPATIBLE_NS_VERSION: '2022.1',
-	SDK_FILENAME: 'cli-2022.1.0-SNAPSHOT.jar',
+	COMPATIBLE_NS_VERSION: '2021.2',
+	SDK_FILENAME: 'cli-2021.2.2.jar',
 	SDK_REQUIRED_JAVA_VERSION: '11',
 	SDK_INTEGRATION_MODE_JVM_OPTION: '-DintegrationMode',
 	SDK_CLIENT_PLATFORM: 'SuiteCloudCLIforNode.js',
