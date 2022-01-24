ff2c418aa7e13db30177b87114ae0d3044589c30
Antonio Fruci <antonio.fruci@oracle.com>
2022-01-24 12:04:20 +0100

    PDPDEVTOOL-4610 code review

--- packages/node-cli/src/core/sdksetup/License.js
+++ packages/node-cli/src/core/sdksetup/License.js
@@ -1,6 +1,6 @@
 /*
- ** Copyright (c) 2022 Oracle and/or its affiliates.  All rights reserved.
- ** Licensed under the FUTC Oracle License v 1.0 as shown at https://www.oracle.com/downloads/licenses/oracle-free-license.html.
+ ** Copyright (c) 2021 Oracle and/or its affiliates.  All rights reserved.
+ ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
 'use strict';
 
