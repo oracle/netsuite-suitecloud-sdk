1f7f057e519ccce6f2de56b30e13c6daa375e404
Antonio Fruci <antonio.fruci@oracle.com>
2022-01-31 15:53:49 +0100

    PDPDEVTOOL-4610 add --acceptSuiteCloudSDKLicense flag to skip question prompt

--- packages/node-cli/preinstall.js
+++ packages/node-cli/preinstall.js
@@ -1,15 +1,18 @@
 'use strict';
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 const { spawn } = require('child_process');
 
 const LICENSE_PATH = path.normalize('./resources/FUTC-LICENSE.txt');
+const LICENSE_FILE_ENCODING = 'utf8';
 const WINDOWS_PLATFORM = 'win32';
 const WINDOWS_SHELL = 'cmd';
 const WINDOWS_SHELL_PARAMS = ['/c', 'more', LICENSE_PATH];
 const UNIX_SHELL = 'sh';
 const UNIX_SHELL_PARAMS = ['-c', `less -meX ${LICENSE_PATH}`];
+const REJECT_EXIT_CODE = 1;
 
 const RED_COLOR = '\x1b[31m';
 const COLOR_RESET = '\x1b[0m';
@@ -18,30 +21,38 @@ const INSTALLATION_MESSAGE = 'The installation will download the SuiteCloud SDK 
 	'for this package. By downloading the SuiteCloud SDK dependency,\n' +
 	'you are accepting the Oracle Free Use Terms and Conditions license\n' +
 	'displayed above.';
+const ACCEPT_FLAG_MESSAGE = 'By providing --acceptSuiteCloudSDKLicense flag you confirmed that you have\n' +
+	'read and accepted the Oracle Free Terms and Conditions license\nfor the SuiteCloud SDK displayed above.'
 const QUIT_MESSAGE = 'To continue with the installation, the previously displayed license must be accepted.';
 const ABORT_MESSAGE = 'Installation aborted by user.';
 const NEGATIVE_ANSWERS = ['n', 'no'];
 const AFFIRMATIVE_ANSWERS = ['y', 'yes'];
-const REJECT_EXIT_CODE = 1;
-
+const LICENSE_NOT_FOUND_ERROR = 'The FUTC-LICENSE.txt file is missing and cannot be displayed. Please try again.';
 
 (() => {
-	//TODO: define how to skip license to be accepted when running in CI
-	// if (process.env.ACCEPT_SUITECLOUD_SDK_LICENSE) {
-	// 	process.exit();
-	// }
 	return showLicense();
 })();
 
 function showLicense() {
+	if (process.env.npm_config_acceptSuiteCloudSDKLicense) {
+		try {
+			const license = fs.readFileSync(LICENSE_PATH, LICENSE_FILE_ENCODING);
+			console.log(license);
+			return printMessage(ACCEPT_FLAG_MESSAGE);
+		} catch (err) {
+			throw new Error(LICENSE_NOT_FOUND_ERROR);
+		}
+	}
+
 	let shell;
 	if (os.platform() === WINDOWS_PLATFORM) {
 		shell = spawn(WINDOWS_SHELL, WINDOWS_SHELL_PARAMS, { stdio: 'inherit' });
 	} else {
 		shell = spawn(UNIX_SHELL, UNIX_SHELL_PARAMS, { stdio: 'inherit' });
 	}
+
 	shell.on('close', (code) => {
-		if(code === 0) {
+		if (code === 0) {
 			return promptQuestion();
 		}
 		return process.exit(code);
@@ -51,12 +62,7 @@ function showLicense() {
 	});
 }
 
-
 function promptQuestion() {
-
-	function printMessage(message) {
-		console.log(os.EOL + RED_COLOR + message + COLOR_RESET + os.EOL);
-	}
 
 	const rl = readline.createInterface({
 		input: process.stdin,
@@ -90,4 +96,8 @@ function promptQuestion() {
 			return askQuestion();
 		});
 	})();
+}
+
+function printMessage(message) {
+	console.log(os.EOL + RED_COLOR + message + COLOR_RESET + os.EOL);
 }
