1773c0092f99ada2165c0b97dc3f40a293506921
Carol Navarro Pereira <carol.navarro.pereira@oracle.com>
2026-04-02 16:54:04 +0200

    temp commit 3

--- packages/node-cli/src/commands/proxy/generatekey/ProxyGenerateKeyAction.js
+++ packages/node-cli/src/commands/proxy/generatekey/ProxyGenerateKeyAction.js
@@ -7,8 +7,14 @@
 const { ActionResult } = require('../../../services/actionresult/ActionResult');
 const BaseAction = require('../../base/BaseAction');
 const { generateApiKey } = require('../../../utils/APIKeyGenerator');
+const SdkExecutionContext = require('../../../SdkExecutionContext');
+const { executeWithSpinner } = require('../../../ui/CliSpinner');
+const NodeTranslationService = require('../../../services/NodeTranslationService');
+const SdkOperationResultUtils = require('../../../utils/SdkOperationResultUtils');
 
 const PROXY_KEY = 'PROXY_KEY';
+const READ_CLIENT_API_CONTENT_CLI_COMMAND = 'readclientapikeycontent';
+const WRITE_CLIENT_API_CONTENT_CLI_COMMAND = 'writeclientapicontent';
 
 
 module.exports = class ProxyGenerateKeyAction extends BaseAction {
@@ -18,13 +24,86 @@ module.exports = class ProxyGenerateKeyAction extends BaseAction {
 
 	async execute() {
 		try {
+			//1.- Get the current contents of client_api_key.p12
+			const clientApiContents = await this._readClientAPIContents();
+			//2.- Generate the API Key
 			const apiKey = generateApiKey();
-			const proxyKeyJson = JSON.stringify({ [PROXY_KEY]: apiKey });
-			//TODO read apiKey and check whether PROXY_KEY exists or not, if it does not add
-			//if it does replace by PROXY_KEY
-			return ActionResult.Builder.withData({ apiKey: proxyKeyJson }).build();
-		} catch (error) {
-			return ActionResult.Builder.withErrors([error]).build();
+			//3.- Generate/update the new JSON Api Key
+			const newClientApiContent = this._modifyClientApiContentsWithNewKey(clientApiContents, apiKey);
+
+			const updatedClientApiContentsJson = this._normalizeJsonString(newClientApiContent);
+			await this._writeClientAPIContents(updatedClientApiContentsJson);
+
+			return ActionResult.Builder.withData({ apiKey: apiKey }).build();
+		} catch (err_) {
+			return ActionResult.Builder.withErrors(Array.isArray(err_) ? err_ : [err_]).build();
 		}
+	}
+
+	_normalizeJsonString(jsonString) {
+		return JSON.stringify(jsonString).replaceAll('"', String.raw`\"`);
+	}
+
+	_modifyClientApiContentsWithNewKey(clientApiContents, newApiKey) {
+		try {
+			const newClientApiContents = (clientApiContents) ?
+				JSON.parse(clientApiContents)
+				: {};
+
+			if (Object.hasOwn(newClientApiContents, PROXY_KEY)) {
+				delete newClientApiContents[PROXY_KEY];
+			}
+
+			newClientApiContents[PROXY_KEY] = {
+				creationDate: new Date().toISOString(),
+				apiKey: newApiKey,
+			};
+			return newClientApiContents;
+
+		} catch (err) {
+			throw ['Client API contents must be a valid format. Please, delete client_api_key.p12 file and try again', err];
+		}
+	}
+
+	async _readClientAPIContents() {
+		const executionContext = SdkExecutionContext.Builder.forCommand(READ_CLIENT_API_CONTENT_CLI_COMMAND)
+			.integration()
+			.addFlags([])
+			.build();
+
+		const operationResult = await executeWithSpinner({
+			action: this._sdkExecutor.execute(executionContext),
+			message: 'Reading Client API Contents', //TODO move to language files
+		});
+
+		if (operationResult.errorMessages && operationResult.errorMessages.length > 0) {
+			throw operationResult.errorMessages;
+		}
+
+		if (operationResult.status === SdkOperationResultUtils.STATUS.SUCCESS) {
+			return operationResult.data;
+		} else {
+			throw new Error('Error reading Client API contents');  //TODO move to language files
+		}
+	}
+
+	async _writeClientAPIContents(proxyKeyJson) {
+		const executionContext = SdkExecutionContext.Builder.forCommand(WRITE_CLIENT_API_CONTENT_CLI_COMMAND)
+			.integration()
+			.addParam('content', proxyKeyJson) //TODO content to a const
+			.build();
+		const operationResult = await executeWithSpinner({
+			action: this._sdkExecutor.execute(executionContext),
+			message: 'Writing Client API Contents', //TODO move to language files
+		});
+
+		if (operationResult.errorMessages && operationResult.errorMessages.length > 0) {
+			throw operationResult.errorMessages;
+		}
+
+		if (operationResult.status !== SdkOperationResultUtils.STATUS.SUCCESS) {
+			throw new Error('Error reading writing API contents');  //TODO move to language files
+		}
+
 	}
 };
